9240174c208481325fe63903c49e3c8f401b59a6
leac1m <obemichael10@gmail.com>
2025-04-01 10:31:55 +0100

    handled "if user exists" in registering user

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
+    const mockUsersService = {
+      createUser: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should throw a 409 error if user already exists', async () => {
+    const walletAddress = '0x123456789';
+    const username = 'JohnDoe';
+
+    // Mock the service to throw an HttpException with 409 status
+    jest.spyOn(service, 'createUser').mockImplementation(() => {
+      throw new HttpException(
+        'User with this wallet address already exists',
+        HttpStatus.CONFLICT,
+      );
+    });
+
+    try {
+      await controller.register({ walletAddress, username });
+    } catch (e) {
+      if (e instanceof HttpException) {
+        expect(e.getStatus()).toBe(HttpStatus.CONFLICT);
+        expect(e.message).toBe('User with this wallet address already exists');
+      } else {
+        // Fail the test if the error is not an HttpException
+        fail(`Unexpected error type: ${e}`);
+      }
+    }
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -10,6 +10,17 @@ export class UsersService {
     }
 
     async createUser(walletAddress: string, username: string) {
+        const existingUser = await this.prisma.prismaClient.User.findUnique({
+            where: { walletAddress },
+        })
+
+        if (existingUser) {
+            throw new HttpException(
+                'User with this wallet address already exists',
+                HttpStatus.CONFLICT,
+            );
+        }
+
         return this.prisma.prismaClient.user.create({
             data: { walletAddress, username },
         });
