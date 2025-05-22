0be579da666fe7b3097cbb6007de9f8971afd54a
blockchainBard101 <bchainbard.annonymousasquare@gmaol.com>
2025-05-22 15:58:16 +0100

    updated corpss

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [CardsService],
-  controllers: [CardsController]
+  controllers: [CardsController],
+  imports: [PrismaModule],
 })
 export class CardsModule {}
